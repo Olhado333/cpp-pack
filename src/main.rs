5a2348b6ed39ddb20fa303ea392ecb3797db6009
4812571 <micah.harrington9@gmail.com>
2023-10-19 18:14:30 -0500

    converted to use iterator methods

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,16 @@ mod write_zip;
 fn main() {
     let root_directory: PathBuf = current_dir().expect("Could not find directory.");
 
-    let mut paths: Vec<PathBuf> = Vec::new();
-
-    for item in fs::read_dir(&root_directory).expect("Failed to read directory.") {
-        let path = item.expect("Failed to read file.").path();
-        let file_extention = match path.extension() {
-            None => continue,
-            Some(ext) => ext,
-        };
-
-        if file_extention == "cpp" || file_extention == "h" {
-            paths.push(path);
-        }
-    }
+    let paths: Vec<PathBuf> = fs::read_dir(&root_directory).expect("Failed to read directory.")
+        .filter_map(|x| x.ok())
+        .map(|x| x.path())
+        .filter(|x| {
+            match x.extension() {
+                None => false,
+                Some(ext) => ext == "cpp" || ext == "h",
+            }
+        })
+        .collect();
 
     write_zip::copy_to_zip(root_directory.join("submission").with_extension("zip"), &paths);
 }
